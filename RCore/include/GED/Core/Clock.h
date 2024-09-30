630eb51aa58ae45e4b93120b3837e2785064a2a8
Yui San ae2f <mincraft20046@gmail.com>
2024-09-30 18:37:38 +0900

    File Name Typo Hotfix

--- RCore/include/GED/Core/Clock.h
+++ RCore/include/GED/Core/Clock.h
@@ -1,6 +1,8 @@
 #if !defined(GED_Core_Clock_h)
 #define GED_Core_Clock_h
 #include <ae2f/Macro/Cast.h>
+#include <ctime>
+
 
 ae2f_extern time_t GED_Core_Clock_Now(time_t NewCastDuration = 0);
 #endif // !defined(GED_Core_Clock_h)
