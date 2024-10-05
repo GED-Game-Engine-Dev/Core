9e4e7dc0a9216d7eb62af58bde203280e48bdfa7
Yui San ae2f <mincraft20046@gmail.com>
2024-10-05 12:55:15 +0900

    .

--- GED_CORE/Ctrl/EventCircularQ.cs
+++
@@ -1,8 +0,0 @@
-using System.Runtime.InteropServices;
-
-namespace GED.Core.Ctrl {
-    public class iEventCircularQ<T> : xEvent<T> where T : struct {
-        public iEventCircularQ(UIntPtr size, out int perr)
-        : base(size, out perr) {}      
-    }
-}
