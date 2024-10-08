6ddd78d121b077e5f9d5535bca08f10fdc829aea
Yui San ae2f <84163330+yuisanae2f@users.noreply.github.com>
2024-10-08 22:26:18 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,14 +1,15 @@
-rm -rf ../build
+rm -rf ./build
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    cmake -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_C_COMPILER:STRING=gcc -DCMAKE_CXX_COMPILER:STRING=g++ -S../ -B../build
+    cmake -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_C_COMPILER:STRING=gcc -DCMAKE_CXX_COMPILER:STRING=g++ -S./ -B./build
 
 elif [[ "$OSTYPE" == "cygwin" ]]; then
-    cmake -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_C_COMPILER:STRING=gcc -DCMAKE_CXX_COMPILER:STRING=g++ -S../ -B../build -G "MinGW Makefiles"
+    cmake -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_C_COMPILER:STRING=gcc -DCMAKE_CXX_COMPILER:STRING=g++ -S./ -B./build -G "MinGW Makefiles"
 elif [[ "$OSTYPE" == "msys" ]]; then
-    cmake -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_C_COMPILER:STRING=gcc -DCMAKE_CXX_COMPILER:STRING=g++ -S../ -B../build -G "MinGW Makefiles"
+    cmake -DCMAKE_BUILD_TYPE:STRING=Release -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_C_COMPILER:STRING=gcc -DCMAKE_CXX_COMPILER:STRING=g++ -S./ -B./build -G "MinGW Makefiles"
 else
     echo "We can't give that $OSTYPE"
 fi
 
-cmake --build ../build --config Release
+cmake --build ./build --config Release
+rm -rf ./build
