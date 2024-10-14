4230282ebe9896caa78e79f1e2c47807eaf9a95d
Cream325 Chi <pto5515@gmail.com>
2024-10-14 23:22:07 +0900

    1. MainViewModel     - ConvertBitmap() 함수 추가
    2. MainWindow
        - DataTemplate 추가 (아직은 쓰이지 않음, 오류 있음)

--- test/ViewModels/MainViewModel.cs
+++ test/ViewModels/MainViewModel.cs
@@ -12,6 +12,7 @@ namespace test.ViewModels
     {
         #region Memver Fields
         private ContentControl contentControl;
+        private Bitmap bitmapImage;
 
         public event PropertyChangedEventHandler? PropertyChanged;
         #endregion
@@ -26,6 +27,16 @@ namespace test.ViewModels
                 OnPropertyChanged(nameof(value));
             }
         }
+
+        public Bitmap BitmapImage
+        {
+            get => bitmapImage;
+            set
+            {
+                bitmapImage = value;
+                OnPropertyChanged(nameof(value));
+            }
+        }
         #endregion
 
         public MainViewModel()
@@ -33,11 +44,30 @@ namespace test.ViewModels
             int err;
             var content = new Content(out err, 1920, 1080);
             ContentControl = content;
+            BitmapImage = ConvertBitmap(content.DisplayBuffer);
         }
 
         protected void OnPropertyChanged(string propertyName)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// WriteableBitmap을 Bitmap으로 변환해주는 함수
+        /// </summary>
+        /// <param name="writeableBitmap"></param>
+        /// <returns></returns>
+        private Bitmap? ConvertBitmap(WriteableBitmap writeableBitmap)
+        {
+            Bitmap? bitmap = null;
+            using(var stream = new MemoryStream())
+            {
+                writeableBitmap.Save(stream);
+                stream.Seek(0, SeekOrigin.Begin);
+                bitmap = new Bitmap(stream);
+            }
+
+            return bitmap;
         }
     }
 }
