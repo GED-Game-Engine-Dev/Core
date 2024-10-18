eb669780e954842049b88dc78c980970fcf167ba
Yui San ae2f <mincraft20046@gmail.com>
2024-10-19 01:15:23 +0900

    Test update

--- test/MainWindow.axaml.cs
+++ test/MainWindow.axaml.cs
@@ -1,7 +1,7 @@
 using System;
+using System.Diagnostics;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
-using Avalonia.Media.Imaging;
 using GED.Core;
 using GED.Core.DisplayWizard;
 
@@ -11,25 +11,47 @@ namespace test
     {
         public Camera camera;
 
-        public MainWindow(out int err) : base(out err, 48, 48)
+        public MainWindow(out int err) : base(out err, 1920, 1080)
         {
             InitializeComponent();
+
             Buffer = this.FindControl<Image>("MyImage");
             camera = new Camera(out err);
-            camera.Resize(1);
+            camera.Resize(10);
             GED.Core.Manager.Bitmap.EmplaceBack(Resource1.Bitmap1);
 
             BmpSource source;
             err = GED.Core.Manager.Bitmap.GetSource(0, out source);
-            Camera.Element element = new(out err, 255, 48, 48, 0, 0, 1, in source);
+
+            // Stopwatch 객체 생성
+            Stopwatch stopwatch = new Stopwatch();
 
 
-            camera.Write(0, in element);
+            // Implemented as not Stretch, but trim
+            Camera.Element[] element = {
+                new(out err, 255, 1920, 1080, 0, 0, 1, in source),
+                new(out err, 40, 40, 200, 49, 500, 1, in source),
+                new(out err, 100, 90, 80, 500, 100, 1, in source),
+            };
+
+            for(int i = 0; i < 3; i++) {
+                camera.Write((uint)i, in element[i]);
+            }
+
+            stopwatch.Start();
+
             try {
-                camera.BuffAll(DisplayBuffer, 0x0);
+                camera.BuffAll(DisplayBuffer, 0);
             } catch(Exception e) {
                 Console.WriteLine(e);
             }
+
+            // 시간 측정 종료
+            stopwatch.Stop();
+
+            // 경과 시간 출력
+            Console.WriteLine("경과 시간: " + stopwatch.ElapsedMilliseconds + " ms");
+
         }
 
         void InitializeComponent() {
