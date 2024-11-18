741bd273671534698df09db64bfdcf26a960f976
YuiSanae2f <mincraft20046@gmail.com>
2024-11-19 04:52:25 +0900

    Rel-v0.4.0.1

--- test/MainWindow.axaml.cs
+++ test/MainWindow.axaml.cs
@@ -44,7 +44,7 @@ namespace test
                 while(true) {
                     stopwatch.Restart();
                     element.CheckPrm(out err).RotateXYClockWise = i / 10.0;
-                    element.CheckPrm(out err).WidthAsResized = (uint)i * 5 + 1;
+                    // element.CheckPrm(out err).WidthAsResized = (uint)i * 5 + 1;
                     element.CheckPrm(out err).ReverseIdx = (byte)(i % 3);
                     camera.Write((uint)0, in element);
                     i++;
