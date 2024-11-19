d9b0ef76bcc9045929f14727f96f8f56557e841f
YuiSanae2f <mincraft20046@gmail.com>
2024-11-20 04:54:30 +0900

    Rel-v0.5.1
    New Float type has been generated

--- GED_CORE/Ctrl/MousePoint.cs
+++ GED_CORE/Ctrl/MousePoint.cs
@@ -3,23 +3,23 @@ using System.Runtime.InteropServices;
 namespace GED.Core.Ctrl
 {
     internal static unsafe partial class fMousePoint {
-        [LibraryImport(SanityCheck.DllNames.RCore, EntryPoint = "GED_Core_Ctrl_MousePoint_ptrX")]
-        private static partial double* pX();
+        [LibraryImport(SanityCheck.DllNames.RCore, EntryPoint = "GED_Ctrl_MousePoint_ptrX")]
+        private static partial SanityCheck.Float* pX();
         
-        [LibraryImport(SanityCheck.DllNames.RCore, EntryPoint = "GED_Core_Ctrl_MousePoint_ptrY")]
-        private static partial double* pY();
+        [LibraryImport(SanityCheck.DllNames.RCore, EntryPoint = "GED_Ctrl_MousePoint_ptrY")]
+        private static partial SanityCheck.Float* pY();
 
-        public static double* X = pX();
-        public static double* Y = pY();
+        public static SanityCheck.Float* X = pX();
+        public static SanityCheck.Float* Y = pY();
     }
 
     public static unsafe class MousePoint
     {
-        public static double X {
+        public static SanityCheck.Float X {
             get { return fMousePoint.X[0]; }
         }
 
-        public static double Y {
+        public static SanityCheck.Float Y {
             get { return fMousePoint.Y[0]; }
         }
     }
